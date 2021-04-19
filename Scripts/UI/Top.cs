712fbb9f060d9e0467ec6e625c2375e7cc9d35e5
89trillion-shenhua <76935887+89trillion-shenhua@users.noreply.github.com>
2021-04-19 10:20:10 +0800

    Add files via upload

---
+++ Scripts/UI/Top.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Top : MonoBehaviour
+{
+    [SerializeField] private Text coinText; 
+    
+    public void Init()
+    {
+        coinText.text = GameData.Instance.coinsCount.ToString();
+    }
+}
